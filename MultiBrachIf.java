718f70e2af1b4e8cf3a5c134f54a7203262b0bc7
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:29:15 +0300

    Add files via upload

---
+++ MultiBrachIf.java
@@ -0,0 +1,35 @@
+package week_04;
+
+public class MultiBrachIf {
+    public static void main(String[] args) {
+        // if the number is positive print possitive
+        // if the number is negative print negative
+        // else print 0.
+
+        int number=-1100*8/2-100;
+
+        if (number>0){
+            System.out.println("possitive");
+        }
+        else if (number<0){
+            System.out.println("negative");
+        }
+        else {
+            System.out.println("zero");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
